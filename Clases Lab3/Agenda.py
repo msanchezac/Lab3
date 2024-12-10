6796329b36bb86d8851d775f551ae3ef218a715a
SebastiánMartínez <159493363+S3bastian2@users.noreply.github.com>
2024-12-10 09:37:00 -0500

    Metodo buscar listo

--- Clases Lab3/Agenda.py
+++ Clases Lab3/Agenda.py
@@ -5,3 +5,13 @@ class Agenda():
         self.__registro = []
         self.__no_reg = 0
 
+def agregar(Usuario u):
+        pass
+
+    def buscar(id): #Método buscar, recibe como parametros el id del usuario
+        id_buscado = id #Variable que almacena el id que queremos comparar
+        for ids in range(no_reg): #Ciclo que recorre el arreglo hasta los objetos que hayamos decidido meter en el
+            if __registro[ids].id == id_buscado: #Comparamos cada id de los objetos con el id presentado
+                return ids #Si la comparación es verdadera entonces retornamos el indice de dicho objeto
+        else: #En caso de no haberse encontrado comparación similar
+            return -1 #Retornamos menos uno
