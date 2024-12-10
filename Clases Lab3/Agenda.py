3af7a2a909af14c8b4908a8f0a2b66f68d1e5d40
SebastiánMartínez <159493363+S3bastian2@users.noreply.github.com>
2024-12-10 10:59:59 -0500

    Agregando comentarios

--- Clases Lab3/Agenda.py
+++ Clases Lab3/Agenda.py
@@ -6,12 +6,12 @@ class Agenda():
         self.__no_reg = 0
 
 def agregar(Usuario u):
-        if buscar(u.getId()) != -1:
-            return False
+        if buscar(u.getId()) != -1: #Tomamos el id del usuario y se lo pasamos al método buscar, si este obtiene un valor diferente de menos 1 retornara False
+            return False #Retorna False pues el objeto ya pertenece al arreglo
         else:
-            __registro.append(u)
-            no_reg += 1
-            return True
+            __registro.append(u) #Si el valor retornado fue menos uno, entonces se agrega el nuevo objeto al arreglo
+            no_reg += 1 #Aumenta el tamaño del arreglo
+            return True #Retorna True porque el nuevo objeto fue agregado con exito
 
     def buscar(id): #Método buscar, recibe como parametros el id del usuario
         id_buscado = id #Variable que almacena el id que queremos comparar
