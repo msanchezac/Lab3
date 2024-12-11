83f97c772b448a0800ea11e68ec92dfb93e4d982
ValePadilla11 <vpadillam@unal.edu.co>
2024-12-11 11:06:26 -0500

    Correciones

--- Clases Lab3/Agenda.py
+++ Clases Lab3/Agenda.py
@@ -2,13 +2,13 @@ from Clases.Usuario import *
 
 class Agenda():
     def __init__(self, registro, no_reg):
-        self.registro = []
-        self.no_reg = 0
+        self.__registro = []
+        self.__no_reg = 0
 
     def buscar(id): #Método buscar, recibe como parametros el id del usuario
         id_buscado = id #Variable que almacena el id que queremos comparar
-        for ids in range(no_reg): #Ciclo que recorre el arreglo hasta los objetos que hayamos decidido meter en el
-            if registro[ids].id == id_buscado: #Comparamos cada id de los objetos con el id presentado
+        for ids in range(__no_reg): #Ciclo que recorre el arreglo hasta los objetos que hayamos decidido meter en el
+            if __registro[ids].id == id_buscado: #Comparamos cada id de los objetos con el id presentado
                 return ids #Si la comparación es verdadera entonces retornamos el indice de dicho objeto
         else: #En caso de no haberse encontrado comparación similar
             return -1 #Retornamos menos uno
@@ -17,8 +17,8 @@ class Agenda():
         if buscar(u.getId()) != -1: #Tomamos el id del usuario y se lo pasamos al método buscar, si este obtiene un valor diferente de menos 1 retornara False
             return False #Retorna False pues el objeto ya pertenece al arreglo
         else:
-            registro.append(u) #Si el valor retornado fue menos uno, entonces se agrega el nuevo objeto al arreglo
-            no_reg += 1 #Aumenta el tamaño del arreglo
+            __registro.append(u) #Si el valor retornado fue menos uno, entonces se agrega el nuevo objeto al arreglo
+            __no_reg += 1 #Aumenta el tamaño del arreglo
             return True #Retorna True porque el nuevo objeto fue agregado con exito
     
     def eliminar(id): #Método que elimina objetos
@@ -26,10 +26,10 @@ class Agenda():
             return False #Retorna Falso porque no se encontraba el objeto
         else: #En caso de que el objeto este
             indice_eliminado = buscar(id)
-            for i in range(indice_eliminado, no_reg-2): #Se empieza a iterar desde el indice del objeto eliminado
-                registro[i] = registro[i+1] #Se mueven los objetos hacia la ¿izquierda?
-            registro[no_reg-1] = None #El ultimo valor del arreglo ya no esta pues todo fue ocupado por los demas valores
-            no_reg -= 1 #Disminuimos el tamaño del arreglo
+            for i in range(indice_eliminado, __no_reg-2): #Se empieza a iterar desde el indice del objeto eliminado
+                __registro[i] = __registro[i+1] #Se mueven los objetos hacia la ¿izquierda?
+            __registro[no_reg-1] = None #El ultimo valor del arreglo ya no esta pues todo fue ocupado por los demas valores
+            __no_reg -= 1 #Disminuimos el tamaño del arreglo
             return True #Devolvemos Verdadero tras haberse completado todo el proceso
                 
 
